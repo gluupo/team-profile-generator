525b71d2e611bc15a5e7fd7e627aee153fb735c2
gluupo <ariannasbanks@gmail.com>
2021-12-19 17:27:57 -0500

    employee class

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+// required packages
 const fs = require('fs');
 const inquirer = require('inquirer')
+
+
+//employee profiles
+class Employee {
+  constructor(name, id, email, role) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = 'Employee'
+  }
+  getName() {
+    return this.name;
+  }
+  getID() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return this.role;
+  }
+}
